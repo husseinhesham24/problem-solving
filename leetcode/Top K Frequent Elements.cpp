848eebe9530e68370e42b7dfffd2e1f2b94dcf4b
husseinhesham24 <husseinhesham24@gmail.com>
2023-05-23 01:27:34 +0300

    leethub script

--- leetcode/Top K Frequent Elements.cpp
+++ leetcode/Top K Frequent Elements.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>h;
-        for(int i=0;i<nums.size();i++)
+        for(auto it:nums)
         {
-            h[nums[i]]++;
+            h[it]++;
         }
-        
+
+        int n = nums.size();
         priority_queue<pair<int,int>>pq;
         for(auto it:h)
         {
             pq.push({it.second, it.first});
         }
-        
+
         vector<int>ans;
         while(k--)
         {
             ans.push_back(pq.top().second);
             pq.pop();
         }
+
         return ans;
     }
 };
